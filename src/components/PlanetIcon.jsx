539ddf6096d37ba23efeebceb93817a24c6b6161
kyhaa <kyhaanderson@gmail.com>
2024-04-09 14:21:41 +1000

    Fixes

--- src/components/PlanetIcon.jsx
+++ src/components/PlanetIcon.jsx
@@ -1,14 +1,15 @@
 import {Image} from "@chakra-ui/react"
 const PlanetIcon = ({name}) => {
     const moons = ["Gilly","Mun","Minmus","Ike","Laythe","Vall","Tylo","Bop","Pol"]
+    console.log(name.toLowerCase())
 
     return (
         <>
             {
                 !moons.includes(name) ?
-                    <Image w={10} src={`assets/system/${name}.webp`} alt={"·"}/>
+                    <Image w={10} src={`assets/system/${name.toLowerCase()}.webp`} alt={name}/>
                     :
-                    <Image w={10} ml={10} src={`assets/system/${name}.webp`} alt={"·"}/>
+                    <Image w={10} ml={10} src={`assets/system/${name.toLowerCase()}.webp`} alt={name}/>
             }
         </>
     )
